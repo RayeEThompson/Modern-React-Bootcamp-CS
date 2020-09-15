435b3f10c8e6113bc2547002b324ddf7112a4ae1
RayeEThompson <rachael.elizabeth.thompson@gmail.com>
2020-09-15 05:54:06 -0500

    docs: :pencil: doc funct component
    document functional component vs class component

--- First_Component/index.js
+++ First_Component/index.js
@@ -1,3 +1,6 @@
+/**
+ * Class Component Style
+ */
 class Hello extends React.Component {
 	render() {
 		return (
@@ -9,5 +12,17 @@ class Hello extends React.Component {
 		);
 	}
 }
+/**
+ * Functional Component Style via Arrow Function
+ */
+// const Hello = () => {
+// 	return (
+// 		<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 		</div>
+// 	)
+// }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
